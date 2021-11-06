427d109c71094616a51e500728ed0ff0aa1c1449
chyd <123@qq.com>
2021-11-06 19:10:55 +0800

    hotfix commint1

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/ConsumerOrder80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/ConsumerOrder80.java
@@ -7,11 +7,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ConsumerOrder80 {
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(ConsumerOrder80.class, args);
         System.out.println("启动成功");
+        System.out.println("hot-fix");
     }
+
 }
