bdee5d6e95c4e6819c07763c98b19ed66c03ddf3
chyd <123@qq.com>
2021-11-06 19:11:48 +0800

    master commint1

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/ConsumerOrder80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/ConsumerOrder80.java
@@ -7,11 +7,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ConsumerOrder80 {
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(ConsumerOrder80.class, args);
         System.out.println("启动成功");
+        System.out.println("master");
     }
 }
